a6cfb66b89fca5d70fa6f784431cd718558616cd
Kainã Terto <kterto@capyba.com>
2023-06-01 17:48:17 -0300

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BlogPost
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'created_at', 'created_by', 'published_at', 'published_by']
+    list_filter = ['is_premium']
+    search_fields = ['title', 'body']
